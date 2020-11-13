9066d982a51f8a40e8cf8b937d0c99f4b39b98f7
Stigmond <stigmond@gmail.com>
2020-11-13 16:11:06 -0700

    Validate JPA / Hibernate config

--- JPASpooky/src/main/java/com/skilldistillery/spooky/entities/Encounter.java
+++ JPASpooky/src/main/java/com/skilldistillery/spooky/entities/Encounter.java
@@ -1,5 +1,76 @@
 package com.skilldistillery.spooky.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Encounter {
+
+	//###FIELDS###
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String title;
+
+	//###CONSTRUCTOR###
+	public Encounter() {
+		super();
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Encounter other = (Encounter) obj;
+		if (id != other.id)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Encounter [id=").append(id).append(", title=").append(title).append("]");
+		return builder.toString();
+	}
 
 }
--- JPASpooky/src/test/java/com/skilldistillery/spooky/entities/EncounterTest.java
+++ JPASpooky/src/test/java/com/skilldistillery/spooky/entities/EncounterTest.java
@@ -1,6 +1,10 @@
 package com.skilldistillery.spooky.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -10,25 +14,35 @@ import org.junit.jupiter.api.Test;
 
 class EncounterTest {
 
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Encounter encounter;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("SpookyPU");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
+		
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		encounter = em.find(Encounter.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
 	}
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		assertEquals("Encounter at Farpoint", encounter.getTitle());
 	}
 
 }
