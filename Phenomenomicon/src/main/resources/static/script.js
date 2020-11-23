3f48179374a716764b09811ce47f219546d35b34
Stigmond <stigmond@gmail.com>
2020-11-22 22:59:17 -0700

    Completed section 4a of requirements with detail view of table

--- Phenomenomicon/src/main/resources/static/script.js
+++ Phenomenomicon/src/main/resources/static/script.js
@@ -3,31 +3,49 @@ window.addEventListener('load', function(e) {
   init();
 });
 
-function init(){
+function init() {
+
   getAllEvents();
   document.addEncounter.add.addEventListener('click', addEncounter);
-  };
-
-function getAllEvents(){
-  let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'api/encounter');
-
-    xhr.onreadystatechange = function(){
-   	if (xhr.readyState === 4) {
-   		if (xhr.status === 200) {
-
-    		let encounters = JSON.parse(xhr.responseText);
-        listEncounters(encounters);
-    		}
-    		else {
-        console.error('Film not found.')
-    		}
-    	}
-    };
-    xhr.send();
 };
 
-function listEncounters(encounters){
+function getAllEvents() {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', 'api/encounter');
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+
+        let encounters = JSON.parse(xhr.responseText);
+        listEncounters(encounters);
+      } else {
+        console.error('Encounters not found.');
+      }
+    }
+  };
+  xhr.send();
+};
+
+function getEventById(e) {
+  let id = e.target.parentElement.firstChild.textContent;
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', `api/encounter/${id}`);
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        let encounter = JSON.parse(xhr.responseText);
+        showEvent(encounter);
+      } else {
+        console.error('Encounter not found.');
+      }
+    }
+  };
+  xhr.send();
+};
+
+function listEncounters(encounters) {
   let events = document.getElementById("encounterList");
   events.textContent = '';
   let eventsTableHead = document.createElement('thead');
@@ -49,18 +67,21 @@ function listEncounters(encounters){
 
   let eventsTableBody = document.createElement('tbody');
 
-  // let eventList = document.createElement('');
-  for(let i = 0; i < encounters.length; i++){
+  for (let i = 0; i < encounters.length; i++) {
     let table = document.createElement('tr');
     let td1 = document.createElement('td');
     let td2 = document.createElement('td');
     let td3 = document.createElement('td');
+    let td4 = document.createElement('button');
     td1.textContent = encounters[i].id
     td2.textContent = encounters[i].date
     td3.textContent = encounters[i].entityType;
+    td4.textContent = 'more';
+    td4.addEventListener('click', getEventById);
     table.appendChild(td1);
     table.appendChild(td2);
     table.appendChild(td3);
+    table.appendChild(td4);
     eventsTableBody.appendChild(table);
   }
   events.appendChild(eventsTableBody);
@@ -70,37 +91,51 @@ function addEncounter(e) {
   e.preventDefault();
   let affirm = confirm("Are you sure you wish to add this Encounter?");
   if (affirm) {
-  let newEncounter = {
-    date: e.target.parentElement.date.value,
-    city: e.target.parentElement.city.value,
-    stateCountry: e.target.parentElement.stateCountry.value,
-    latitude: e.target.parentElement.latitude.value,
-    longitude: e.target.parentElement.longitude.value,
-    entityType: e.target.parentElement.entityType.value,
-    entityUrl: e.target.parentElement.entityUrl.value,
-    body: e.target.parentElement.body.value
-  };
+    let newEncounter = {
+      date: e.target.parentElement.date.value,
+      city: e.target.parentElement.city.value,
+      stateCountry: e.target.parentElement.stateCountry.value,
+      latitude: e.target.parentElement.latitude.value,
+      longitude: e.target.parentElement.longitude.value,
+      entityType: e.target.parentElement.entityType.value,
+      entityUrl: e.target.parentElement.entityUrl.value,
+      body: e.target.parentElement.body.value
+    };
 
-  let xhr = new XMLHttpRequest();
-  xhr.open('POST', 'api/encounter');
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'api/encounter');
 
-  xhr.setRequestHeader("Content-type", "application/json");
+    xhr.setRequestHeader("Content-type", "application/json");
 
-  xhr.onreadystatechange   = function() {
-    if (xhr.readyState === 4) {
-      if (xhr.status == 200 || xhr.status == 201) {
-        let data = JSON.parse(xhr.responseText);
-        console.log(data);
-        e.target.parentElement.reset();
-        init();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status == 200 || xhr.status == 201) {
+          let data = JSON.parse(xhr.responseText);
+          console.log(data);
+          e.target.parentElement.reset();
+          init();
+          showEvent(data);
+        } else {
+          console.log("POST request failed.");
+          console.error(xhr.status + ' :' + xhr.responseText);
+        }
       }
-      else {
-        console.log("POST request failed.");
-        console.error(xhr.status + ' :' + xhr.responseText);
-      }
-    }
-  };
-  var userObjectJson = JSON.stringify(newEncounter);
-  xhr.send(userObjectJson);
-}
+    };
+    var userObjectJson = JSON.stringify(newEncounter);
+    xhr.send(userObjectJson);
+  }
+};
+
+function showEvent(encounter) {
+  console.log(encounter);
+  var encounterDetails = document.getElementById('encounterDetails');
+  encounterDetails.textContent = '';
+  let cardTitle = document.createElement('h3');
+  cardTitle.class= 'card-title';
+  cardTitle.textContent = encounter.entityType;
+  encounterDetails.appendChild(cardTitle);
+  let cardText = document.createElement('p');
+  cardText.class = 'card-text';
+  cardText.textContent = encounter.body;
+  encounterDetails.appendChild(cardText);
 };
